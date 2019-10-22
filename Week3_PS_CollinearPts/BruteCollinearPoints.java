5ee100777c02e991ba8ebc9ca6cad920e3e517eb
karimbounekhla <karim.bounekhla@mail.utoronto.ca>
2019-10-21 21:31:05 -0600

    PS3 WIP

---
+++ Week3_PS_CollinearPts/BruteCollinearPoints.java
@@ -0,0 +1,21 @@
+/**
+ * WORK IN PROGRESS
+ */
+
+public class BruteCollinearPoints {
+
+    // finds all line segments containing 4 points
+    public void BruteCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return null;
+    }
+}
