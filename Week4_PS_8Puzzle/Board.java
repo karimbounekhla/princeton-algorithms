16d9d7ea1f5fe56ede2613b11152578bb46019cb
karimbounekhla <karim.bounekhla@mail.utoronto.ca>
2019-11-11 10:42:13 -0700

    Create Board.java

---
+++ Week4_PS_8Puzzle/Board.java
@@ -0,0 +1,61 @@
+/**
+ * Data type that models an n-by-n board with sliding tiles
+ * https://coursera.cs.princeton.edu/algs4/assignments/8puzzle/specification.php
+ *
+ * Work in Progress
+ */
+
+public class Board {
+
+    // create a board from an n-by-n array of tiles,
+    // where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles) {
+
+    }
+
+    // string representation of this board
+    public String toString() {
+
+    }
+
+    // board dimension n
+    public int dimension() {
+
+    }
+
+    // number of tiles out of place
+    public int hamming() {
+
+    }
+
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan() {
+
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+
+    }
+
+    // a board that is obtained by exchanging any pair of tiles
+    public Board twin() {
+
+    }
+
+    // unit testing
+    public static void main(String[] args) {
+
+    }
+
+}
