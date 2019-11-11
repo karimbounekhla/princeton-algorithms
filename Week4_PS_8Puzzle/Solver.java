d60d23627e85d88d7cb1d9135cc0bd6d9c931adc
karimbounekhla <karim.bounekhla@mail.utoronto.ca>
2019-11-11 15:58:43 -0700

    Implemented A* algorithm

--- Week4_PS_8Puzzle/Solver.java
+++ Week4_PS_8Puzzle/Solver.java
@@ -2,44 +2,108 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.MinPQ;
 
+import java.util.*;
+
 /**
  * This class implements A* search to solve a n-by-n slider puzzle using Priority Queues
  */
 
 public class Solver {
+    private SearchNode solution;
+    private boolean isSolvable;
 
-    private class SearchNode {
+    private class SearchNode implements Comparable<SearchNode> {
         private Board b;
         private int moves;
         private SearchNode prev;
 
-        public void SearchNode(Board b, int moves, SearchNode prev) {
+        public SearchNode(Board b, int moves, SearchNode prev) {
             this.b = b;
             this.moves = moves;
             this.prev = prev;
+        }
+
+        @Override
+        public int compareTo(SearchNode o) {
+            return this.priority() - o.priority();
+        }
+
+        public int priority() {
+            return this.b.hamming() + this.moves;
+        }
+
+        public Board getB() {
+            return b;
+        }
+
+        public int getMoves() {
+            return moves;
+        }
+
+        public SearchNode getPrev() {
+            return prev;
         }
     }
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        if (initial == null) {
+            throw new IllegalArgumentException();
+        }
+
         MinPQ<SearchNode> gameTree = new MinPQ<SearchNode>();
-        
+        gameTree.insert(new SearchNode(initial, 0, null));
+
+        while (true) {
+            SearchNode curr = gameTree.delMin();
+            Board currBoard = curr.getB();
+
+            if (currBoard.isGoal()) {
+                solution = curr;
+                isSolvable = true;
+                break;
+            }
+
+            int moves = curr.getMoves();
+            Board prevBoard;
+            if (moves > 0) {
+                prevBoard = curr.getPrev().getB();
+            } else {
+                prevBoard = null;
+            }
+
+            for (Board nextBoard : currBoard.neighbors()) {
+                if (!nextBoard.equals(prevBoard)) {
+                    gameTree.insert(new SearchNode(nextBoard, moves+1, curr));
+                }
+            }
+        }
+
 
     }
 
-    // is the initial board solvable? (see below)
+    // is the initial board solvable?
     public boolean isSolvable() {
-        return true;
+        return isSolvable;
     }
 
     // min number of moves to solve initial board
     public int moves() {
-        return 0;
+        return solution.getMoves();
     }
 
     // sequence of boards in a shortest solution
     public Iterable<Board> solution() {
-        return null;
+        if (isSolvable() == false) {
+            return null;
+        }
+        LinkedList<Board> sols = new LinkedList<Board>();
+        SearchNode search = solution;
+        while (search != null) {
+            sols.addFirst(search.getB());
+            search = search.getPrev();
+        }
+        return sols;
     }
 
     /**
@@ -49,7 +113,9 @@ public class Solver {
     public static void main(String[] args) {
 
         // create initial board from file
-        In in = new In(args[0]);
+        // In in = new In(args[0]);
+        String inp = "test/puzzle04.txt";
+        In in = new In(inp);
         int n = in.readInt();
         int[][] tiles = new int[n][n];
         for (int i = 0; i < n; i++)
