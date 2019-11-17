28171f1faf60ac95f0c631f1c2f993b37644c5eb
karimbounekhla <karim.bounekhla@mail.utoronto.ca>
2019-11-17 15:52:04 -0700

    Setup Week 5 KD-Trees API

---
+++ Week5_PS_Kd-Trees/PointSET.java
@@ -0,0 +1,57 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+/**
+ * Brute-force implementation that represents a set of point in the unit square, implemented using a Red-Black BST
+ *
+ * Work in Progress
+ */
+public class PointSET {
+
+
+    // construct an empty set of points
+    public PointSET() {
+
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return true;
+    }
+
+    // number of points in the set
+    public int size() {
+        return 0;
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        return true;
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+
+    // unit testing of the methods
+    public static void main(String[] args) {
+
+    }
+
+}
